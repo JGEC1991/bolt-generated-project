ee7809f913b4c861a2db979512419b61e5c2f295
JGEC1991 <89346164+JGEC1991@users.noreply.github.com>
2025-02-19 07:34:08 -0600

    Initial commit from your app

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,22 @@
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import dotenv from 'dotenv';
+    import react from '@vitejs/plugin-react';
+    import dotenv from 'dotenv';
 
-dotenv.config();
+    dotenv.config();
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  define: {
-    'process.env': process.env,
-  },
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3000',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
+    // https://vitejs.dev/config/
+    export default defineConfig({
+      plugins: [react()],
+      define: {
+        'process.env': process.env,
       },
-    },
-  },
-});
+      server: {
+        proxy: {
+          '/api': {
+            target: 'http://localhost:3000',
+            changeOrigin: true,
+            rewrite: (path) => path.replace(/^\/api/, ''),
+          },
+        },
+      },
+    });
